ee09cac45d9921da375abe2d2cba71bcee91b45d
David Sutanto <hello.davidsutanto@gmail.com>
2025-02-08 15:06:57 +0800

    React Element

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createElement } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById("root"));
+const reactElement = createElement("h1", null, "Hello from React Element!");
+
+root.render(reactElement);
